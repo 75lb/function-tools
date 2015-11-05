9c795de2903d094d249097604da397989e3066d9
Lloyd Brookes <lloyd.brookes@gmail.com>
2015-11-05 20:34:27 +0000

    license.. js standard format

--- example/throttle.js
+++ example/throttle.js
@@ -1,18 +1,18 @@
-"use strict";
-var f = require("../");
+'use strict'
+var f = require('../')
 
 /* testFunc should be called maximum every 200ms */
-var lastRun = Date.now();
-function testFunc(n){
-    console.log("%d: %d", n, Date.now() - lastRun);
-    lastRun = Date.now();
+var lastRun = Date.now()
+function testFunc (n) {
+  console.log('%d: %d', n, Date.now() - lastRun)
+  lastRun = Date.now()
 }
 
-var throttled = f.throttle(testFunc, { restPeriod: 200 });
+var throttled = f.throttle(testFunc, { restPeriod: 200 })
 
 /* call the throttled testFunc 30 times */
-var i = 0;
-var interval = setInterval(function(){
-    throttled(++i);
-    if (i === 30) clearInterval(interval);
-}, 10);
+var i = 0
+var interval = setInterval(function () {
+  throttled(++i)
+  if (i === 30) clearInterval(interval)
+}, 10)
